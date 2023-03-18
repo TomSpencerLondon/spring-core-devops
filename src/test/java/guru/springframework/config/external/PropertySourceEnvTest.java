b45511ed2c107c168bbfc9b1ba0b3b7912dee3fe
tom <tomspencerlondon@gmail.com>
2023-03-18 11:25:28 +0000

    Correct PropertySourceTest and PropertySourceEnvTest - add ExternalPropsEnvironment

--- src/test/java/guru/springframework/config/external/PropertySourceEnvTest.java
+++ src/test/java/guru/springframework/config/external/PropertySourceEnvTest.java
@@ -1,31 +1,32 @@
 package guru.springframework.config.external;
 
+
 import guru.springframework.config.external.props.ExternalPropsPropertySourceTestConfig;
 import guru.springframework.jms.FakeJmsBroker;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = ExternalPropsPropertySourceTestConfig.class)
-public class PropertySourceTest {
+public class PropertySourceEnvTest {
 
     @Autowired
     FakeJmsBroker fakeJmsBroker;
+
     @Test
-    public void testPropsSet() {
+    void testPropsSet() {
         assertThat(fakeJmsBroker.getUrl())
                 .isEqualTo("10.10.10.123");
         assertThat(fakeJmsBroker.getPort().intValue())
                 .isEqualTo(3330);
-        assertThat(fakeJmsBroker.getUser())
-                .isEqualTo("Ron");
-        assertThat(fakeJmsBroker.getPassword())
-                .isEqualTo("Burgundy");
+        assertThat(fakeJmsBroker.getUser()).isEqualTo("Ron");
+        assertThat(fakeJmsBroker.getPassword()).isEqualTo("Burgundy");
+
     }
 }
